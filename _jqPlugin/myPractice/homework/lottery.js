b95ba62f20047917c144902d240980c451311e22
KrisYan <krisyan3318@gmail.com>
2022-12-13 18:09:43 +0800

    delete log&comment

--- _jqPlugin/myPractice/homework/lottery.js
+++ _jqPlugin/myPractice/homework/lottery.js
@@ -1,15 +1,15 @@
 
 // 產生 1 ~ 49 的按鈕--
 let numberSelect = $('.number-selector');
+
 for (i = 0; i < 49; i++){
 	let newNumBtn = `<button class="number-btn number-${i + 1}">${i + 1}</button>`;
 	numberSelect.append(newNumBtn);
 }
 
-// 1 ~ 49 數字按鈕增加 click(選數字), dblclick(刪除所選數字)--
+// 1 ~ 49 數字按鈕增加 click(選數字), dblclick(刪除所選數字) 事件--
 // 1. click event--
 $('.number-btn').click(function (){
-	// console.log('btn-selected');
 	if (! $(this).hasClass('btn-selected')){
 		if ($('.btn-selected').length >= 6){
 			// 單擊未選 & 超過 6 個數字: 不選 & 提醒--
@@ -28,7 +28,7 @@ $('.number-btn').click(function (){
 	} 
 })
 
-// 產生 6 個不重複數字的 function--
+// 產生 num 個不重複數字的 function--
 function pickNumbers(num){
 	let autoPicks = [];
 	let array49 = [...Array(49).keys()]; // [0 ~ 48]
@@ -36,30 +36,27 @@ function pickNumbers(num){
 
 	for (i = 0; i < num; i++){
 		let ranIdx = Math.floor( Math.random() * numLeft);
-		// console.log('ranIdx', ranIdx);
 
 		// pick an unrepeated number from 1 to 49--
 		autoPicks.push(array49[ranIdx] + 1);
-		// console.log('number picked', array49[ranIdx]);
+
 		array49[ranIdx] = array49[array49.length - 1 - i];
-		// console.log(array49);
 
 		numLeft -=1
 	}
 
-	// console.log('autoPicks', autoPicks);
 	return autoPicks;
 }
 
 // 自動選號按鈕新增 click --
 $('.auto-pick').click(function (){
 	let playerNums = [];
+
 	// clear picks--
 	$('.number-btn').removeClass('btn-selected');
 
 	// auto picks--
 	playerNums = pickNumbers(6);
-	// console.log(playerNums);
 
 	// color picks--
 	for (i = 0; i < 6; i++){
@@ -69,6 +66,8 @@ $('.auto-pick').click(function (){
 
 // 開獎按鈕新增 click--
 $('.btn-ans').click(function (){
+
+	// check 是否已選 6 個數字--
 	if ($('.btn-selected').length < 6){
 		alert('需選擇6個數字');
 		return
@@ -83,9 +82,15 @@ $('.btn-ans').click(function (){
 		display: 'block'
 	})
 
+	// 開獎期間不能按按鈕--
 	$(this).attr('disabled', true);
+	$('.auto-pick').attr('disabled', true);
+	$('.restart').attr('disabled', true);
+
+	// 產生答案--
 	let ansNums = pickNumbers(7);
 
+	// 球動畫--
 	let balls = $('.ball'),
     	diameter = balls.height(), // diameter of ball
    		perimeter = Math.PI * diameter, // perimeter of ball
@@ -102,6 +107,8 @@ $('.btn-ans').click(function (){
 			clearInterval(itv);
 			checkNum(ansNums);
 			$(this).attr('disabled', false);
+			$('.auto-pick').attr('disabled', false);
+			$('.restart').attr('disabled', false);
 		} else{
 			rotateBall( $(window).width()/2-(diameter*(i)) + 245, ansNums[i] );  // input = distance, 每顆球要走的距離，每球遞減，每球減少一顆球的直徑
 			i++;
@@ -145,12 +152,10 @@ $('.btn-ans').click(function (){
 		playerNums = [...playerNums];
 
 		const WinNormalNums = ansNums.slice(0, 6).filter(item => playerNums.includes(item));
-		console.log('WinNormalNums', WinNormalNums);
+
 		let NumOfWinNormalNums = WinNormalNums.length;
-		console.log('NumOfWinNormalNums', NumOfWinNormalNums);
 
 		const WinSpecialNum = playerNums.includes(ansNums[6]);
-		console.log(WinSpecialNum); 
 
 		let finalResult = '';
 		let resultColor = '#ef233c';
@@ -201,6 +206,7 @@ $('.btn-ans').click(function (){
 			display: ''
 		})
 
+		// 顯示結果--
 		$('.final-result1').append(`
 			一般號中 ${NumOfWinNormalNums}個: ${WinNormalNums.join(', ')}<br>
 			特別號中 ${(WinSpecialNum)? 1:0}個: ${(WinSpecialNum)? ansNums[6]:''}`)
@@ -211,7 +217,6 @@ $('.btn-ans').click(function (){
 	}
 
 })
-
 
 // 重新選號按鈕新增 click--
 $('.restart').click(function (){
